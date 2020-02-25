87f28e43b3cb5087644e8c0f5041bd74f1ac75b6
elynur <elynurrahayu@gmail.com>
2020-02-25 12:56:33 +0700

    Latihan intent 1 (skor bola)

--- app/src/main/java/id/putraprima/skorbola/MatchActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -2,17 +2,84 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MatchActivity extends AppCompatActivity {
+    private TextView homeName, awayName, homeScore, awayScore;
+    private ImageView homeLogo, awayLogo;
+    Uri uri1, uri2;
+    Bitmap bitmap1, bitmap2;
+    String homeTeam, awayTeam;
+    int scoreHome = 0;
+    int scoreAway = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
-        //TODO
-        //1.Menampilkan detail match sesuai data dari main activity
-        //2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
-        //3.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang ke ResultActivity, jika seri di kirim text "Draw"
+        homeName = findViewById(R.id.txt_home);
+        awayName = findViewById(R.id.txt_away);
+        homeLogo = findViewById(R.id.home_logo);
+        awayLogo = findViewById(R.id.away_logo);
+        awayScore = findViewById(R.id.score_away);
+        homeScore = findViewById(R.id.score_home);
+
+        Bundle bundle = getIntent().getExtras();
+
+        homeTeam = bundle.getString("inputHome");
+        awayTeam = bundle.getString("inputAway");
+
+        if (bundle != null){
+            uri1 = Uri.parse(bundle.getString("logoHome"));
+            uri2 = Uri.parse(bundle.getString("logoAway"));
+            bitmap1 = null;
+            bitmap2 = null;
+
+            try {
+                bitmap1 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                bitmap2 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri2);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+            homeName.setText(homeTeam);
+            awayName.setText(awayTeam);
+            homeLogo.setImageBitmap(bitmap1);
+            awayLogo.setImageBitmap(bitmap2);
+    }
+
+    //2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
+    public void handleAddHomeScore(View view) {
+        scoreHome += 1;
+        homeScore.setText(String.valueOf(scoreHome));
+    }
+
+    public void handleAddAwayScore(View view) {
+        scoreAway += 1;
+        awayScore.setText(String.valueOf(scoreAway));
+    }
+
+
+    //3.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang ke ResultActivity, jika seri di kirim text "Draw"
+    public void handleCekHasil(View view) {
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("homeScore", scoreHome);
+        intent.putExtra("awayScore", scoreAway);
+        intent.putExtra("homeName", homeTeam);
+        intent.putExtra("awayName", awayTeam);
+        startActivity(intent);
     }
 }
--- app/src/main/java/id/putraprima/skorbola/ResultActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ResultActivity.java
@@ -3,12 +3,35 @@ package id.putraprima.skorbola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    TextView result, winner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        result = findViewById(R.id.result);
+        winner = findViewById(R.id.winner);
+
+        Bundle bundle = getIntent().getExtras();
+        int homeResult = bundle.getInt("homeScore");
+        int awayResult = bundle.getInt("awayScore");
+        String homeName = bundle.getString("homeName");
+        String awayName = bundle.getString("awayName");
+
+        if (bundle != null){
+            result.setText("Score: "+ String.valueOf(homeResult) + "-" + String.valueOf(awayResult));
+            if (homeResult > awayResult){
+                winner.setText("Team "+ homeName + " Memenangkan pertandingan");
+            } else if (awayResult > homeResult){
+                winner.setText("Team "+ awayName + " Memenangkan pertandingan");
+            } else{
+                winner.setText("Team imbang");
+            }
+        }
     }
 }
